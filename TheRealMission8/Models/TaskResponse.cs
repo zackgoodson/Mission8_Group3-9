88499b997735149788b25dd6a4c425a024bac8d9
McKenna Staley <mnnorton@byu.edu>
2023-02-22 14:09:02 -0700

    setitng up models still need to finish db connection

---
+++ TheRealMission8/Models/TaskResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheRealMission8.Models
+{
+    public class TaskResponse
+    {
+        [Key]
+        [Required]
+        public int TaskID { get; set; }
+        [Required]
+        public string Task { get; set; }
+        public string Due { get; set; }
+        [Required]
+        public int Quadrant { get; set; }
+        public bool Completed { get; set; }
+        public int CategoryID { get; set; }
+        public string Category { get; set; }
+    }
+}
