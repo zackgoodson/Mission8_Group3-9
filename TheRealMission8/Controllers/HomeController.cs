86a5ec8ad7ac7eab7eb72a0917ea77a9cc16e5e3
laladean <laurendeano@gmail.com>
2023-02-22 10:28:06 -1000

    deleted pages, created Quadrants view

--- TheRealMission8/Controllers/HomeController.cs
+++ TheRealMission8/Controllers/HomeController.cs
@@ -11,11 +11,10 @@ namespace TheRealMission8.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            
         }
 
         public IActionResult Index()
@@ -23,15 +22,6 @@ namespace TheRealMission8.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
