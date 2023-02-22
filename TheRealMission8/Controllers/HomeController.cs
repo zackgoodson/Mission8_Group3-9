e045eb9f6a7190c2a1e29527739b2837b71636cc
zackgoodson <zackgoodson@gmail.com>
2023-02-22 14:38:46 -0800

    Controller Main

--- TheRealMission8/Controllers/HomeController.cs
+++ TheRealMission8/Controllers/HomeController.cs
@@ -9,19 +9,89 @@ using TheRealMission8.Models;
 
 namespace TheRealMission8.Controllers
 {
+    private TaskContext _taskContext { get; set; }
     public class HomeController : Controller
     {
 
 
-        public HomeController()
+        public HomeController(TaskContext tasksub)
         {
-
+            _taskContext = tasksub;
         }
 
         public IActionResult Index()
         {
             return View();
         }
+        [HttpGet]
+        public IActionResult Quadrants()
+        {
+            var quad = _taskContext.Responses
+                    .Include(x => x.Category)
+                    .ToList();
+            return View(quad);
+        }
+        [HttpGet]
+        public IActionResult Task ()
+        {
+            ViewBag.Categories = _taskContext.ToList();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Task (TaskResponse tr)
+        {
+            if (ModelState.IsValid)
+            {
+                _taskContext.Add(tr);
+                _taskContext.SaveChanges();
+                return View("Quadrants", tr);
+            }
+            else
+            {
+                ViewBag.Categories = _taskContext.Categories.ToList();
+                return View(tr)
+            }
+        }
+    
+        [HttpGet]
+        public IActionResult Edit (int TaskId)
+        {
+            ViewBag.Categories = _taskContext.Categoris.ToList();
+            var task = _taskContext.Responses.Single(x => x.TaskId == TaskId);
+        }
+        [HttpPost]
+        public IActionResult Edit(update)
+        {
+            if (ModelState.IsValid)
+            {
+                _taskContext.Update(update);
+                _taskContext.SaveChanges();
+
+                return RedirectToAction("Quadrants");
+            }
+            else
+            {
+                ViewBag.Categories = _taskContext.Categories.ToList();
+                return View("Task", update);
+            }
+        }
+        [HttpGet]
+        public IActionResult Delete(int TaskId)
+        {
+            var movie = _taskContext.Responses.Single(x => x.MovieId == TaskId);
+
+            return View(movie);
+        }
+        [HttpPost]
+        public IActionResult Delete(TaskResponse tr)
+        {
+            _taskContext.Responses.Remove(tr);
+            _taskContext.SaveChanges();
+
+            return RedirectToAction("Quadrants");
+        }
+
+
 
     }
 }
